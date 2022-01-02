e69477a5b05aa5cb93926e59683a5b1eb5d70803
김시웅 <siung.kim@42dot.ai>
2022-01-03 01:04:31 +0900

    encoding modified

--- apiExtractor.py
+++ apiExtractor.py
@@ -7,11 +7,12 @@ source = sys.argv[1]
 
 if 'http' in sys.argv[1]:
     headers = {"Sec-Ch-Ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"96\"", "Accept": "application/json, text/plain, */*", "Sec-Ch-Ua-Mobile": "?0", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36", "Sec-Ch-Ua-Platform": "\"Windows\"", "Sec-Fetch-Site": "same-origin", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Dest": "empty", "Referer": "https://new-m.pay.naver.com/historybenefit/paymenthistory", "Accept-Encoding": "gzip, deflate", "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7"}
-    c = requests.get(source, headers=headers).text   
+    c = requests.get(source, headers=headers).content
+    c = c.decode('utf-8')
     path = urlparse(sys.argv[1]).path
     path = path[1:].split('?')[0]
     path = path.replace('/','--')
-    f = open(path,'w')
+    f = open(path,'w',encoding='utf-8')
     f.write(c)
     f.close()
     
